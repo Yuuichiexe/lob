079426a3edbf9eba55324d5840269fc34dcc8be4
「 MyaavBoi™ 」 <115485004+Awesome-Gtash@users.noreply.github.com>
2023-02-02 15:24:39 +0530

    Update config.py

--- ShikiXMusic/config.py
+++ ShikiXMusic/config.py
@@ -1,19 +1,19 @@
 from os import getenv
 
 que = {}
-SESSION_NAME = getenv("SESSION_NAME", "session")
+STRING_SESSION = getenv("STRING_SESSION", "session")
 BOT_TOKEN = getenv("BOT_TOKEN")
 BOT_NAME = getenv("BOT_NAME")
-UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "DaisyXupdates")
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "ShikiXupdates")
 BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/dcfdf612e499eef0e0b1f.png")
 admins = {}
 API_ID = int(getenv("API_ID", ""))
 API_HASH = getenv("API_HASH")
 BOT_USERNAME = getenv("BOT_USERNAME")
-ASSISTANT_NAME = getenv("ASSISTANT_NAME", "DaisyXhelper")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "DaisySupport_Official")
-PROJECT_NAME = getenv("PROJECT_NAME", "DaisyXMusic v6")
-SOURCE_CODE = getenv("SOURCE_CODE", "github.com/TeamDaisyX/DaisyXMusic")
+ASSISTANT_USERNAME = getenv("ASSISTANT_USERNAME", "Shiki_Assistant")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "ShikiSupportChat")
+PROJECT_NAME = getenv("PROJECT_NAME", "ShikiXMusic v0.1")
+SOURCE_CODE = getenv("SOURCE_CODE", "github.com/Awesome-Gtash/ShikiXMusic")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "300"))
 PMPERMIT = getenv("PMPERMIT", None)
 LOG_GRP = getenv("LOG_GRP", None)
